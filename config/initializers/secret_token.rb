8cf0f20eda2e96548481fc92e7f7d80dfbb060fe
Zain Raza <zain@mobdub.com>
2011-02-15 22:33:37 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IncentableV2::Application.config.secret_token = 'fc55a90faa8d14b2bf197ff5759346109149b3333a987c19e270e786d50d8eeb2b1ce778aa349f3174effa0c16a4a1e7df2e4cc6d7458d1081a102d612d2b2f6'
